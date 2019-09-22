72c11d5f81728b762cf8609ca3cc5c93ec0b76fd
Tong Yu <yutongsf@gmail.com>
2019-09-22 02:44:57 -0700

    Set Contacts Array As State In App

--- src/App.js
+++ src/App.js
@@ -1,37 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ListContacts from './ListContacts';
-import FavMovie from './data';
+// import FavMovie from './data';
 // import MovieCard from './MovieCard'
 
-const contacts = [
-  {
-    "id": "karen",
-    "name": "Karen Isgrigg",
-    "handle": "karen_isgrigg",
-    "avatarURL": "http://localhost:5001/karen.jpg"
-  },
-  {
-    "id": "richard",
-    "name": "Richard Kalehoff",
-    "handle": "richardkalehoff",
-    "avatarURL": "http://localhost:5001/richard.jpg"
-  },
-  {
-    "id": "tyler",
-    "name": "Tyler McGinnis",
-    "handle": "tylermcginnis",
-    "avatarURL": "http://localhost:5001/tyler.jpg"
-  }
- ];
+class App extends Component {
+  state={
+    contacts: [
+      {
+        "id": "karen",
+        "name": "Karen Isgrigg",
+        "handle": "karen_isgrigg",
+        "avatarURL": "http://localhost:5001/karen.jpg"
+      },
+      {
+        "id": "richard",
+        "name": "Richard Kalehoff",
+        "handle": "richardkalehoff",
+        "avatarURL": "http://localhost:5001/richard.jpg"
+      },
+      {
+        "id": "tyler",
+        "name": "Tyler McGinnis",
+        "handle": "tylermcginnis",
+        "avatarURL": "http://localhost:5001/tyler.jpg"
+      }
+     ]
+    }
 
-function App() {
-  return (
-    <div className="App">
-      <ListContacts contacts={contacts}/>
-      {/* <FavMovie /> */}
-    </div>
-  );
-}
+    render(){
+      return (
+        <div className="App">
+          <ListContacts contacts={this.state.contacts}/>
+          {/* <FavMovie /> */}
+        </div>
+      )
+    };
+  }
 
 export default App;
 
