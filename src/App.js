4f513641a352e491b0fedc3c5fac498e25ef7c7c
Tong Yu <yutongsf@gmail.com>
2019-09-21 22:22:59 -0700

    React component first try

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+class ContactList extends React.Component {
+  render() {
+    const people = this.props.contacts
+
+    return <ol>
+      {people.map((person) => (
+        <li key={person.name}>{person.name}</li>
+      ))}
+    </ol>
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ContactList contacts={[
+          { name: 'Tyler '},
+          { name: 'Karen' },
+          { name: 'Richard '}
+        ]}/>
+        <ContactList contacts={[
+          { name: 'Amanda' },
+          { name: 'Mikenzi' },
+          { name: 'Ryan' }
+        ]}/>
     </div>
   );
 }
 
 export default App;
+
+// props - pass data into your components
+// functional components 
+// controlled components - hookup the forms in your app to your components state 
